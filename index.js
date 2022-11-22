a91f3903f7607f1b7092604ab2ec1b34eea304cd
banelmikhael <banel.mikhael.bm@gmail.com>
2022-11-22 15:40:33 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+class Polygon {
+  constructor(arr) {
+    this.arr = arr;
+  }
+
+  get countSides() {
+    return this.arr.length;
+  }
+
+  get perimeter() {
+    let p = 0;
+    for (let i = 0; i < this.arr.length; i++) {
+      p += this.arr[i];
+    }
+    return p;
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    let isValid2;
+    if (
+      this.arr[0] + this.arr[1] > this.arr[2] &&
+      this.arr[1] + this.arr[2] > this.arr[0] &&
+      this.arr[0] + this.arr[2] > this.arr[1]
+    ) {
+      isValid2 = true;
+    } else {
+      isValid2 = false;
+    }
+    return isValid2;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    let isValid2;
+    if (
+      this.arr[0] === this.arr[1] &&
+      this.arr[0] === this.arr[2] &&
+      this.arr[0] === this.arr[3]
+    ) {
+      isValid2 = true;
+    } else {
+      isValid2 = false;
+    }
+    return isValid2;
+  }
+  get area() {
+    const perimeter = this.perimeter;
+    const area = perimeter / 4;
+    return area * area;
+  }
+}
